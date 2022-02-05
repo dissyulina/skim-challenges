291fbbe7e749f8da6939f6ebdbf5097c169b85f9
Dissy Ulina <dissyulina@gmail.com>
2022-02-05 15:32:38 +0000

    Add input validation and reset functionality to challenge one

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@
 let inputOne = document.getElementById("input1");
 let outputOne = document.getElementById("output1");
 let calculateButtonOne = document.getElementById("calculate-button1");
+let resetButtonOne = document.getElementById("reset-button1");
 
 // Challenge #1 Functionality
 function multiplyByDecrement(input) {
@@ -14,5 +15,15 @@ function multiplyByDecrement(input) {
 
 calculateButtonOne.addEventListener('click', function() {
     let inputValue = Number(inputOne.value);
-    outputOne.innerHTML = multiplyByDecrement(inputValue);
+    // Validate input, an integer between 1-18
+    if ((inputValue >= 1) && (inputValue <= 18) && (Number.isInteger(inputValue))) {
+        outputOne.innerHTML = multiplyByDecrement(inputValue);
+    } else {
+        outputOne.innerHTML = "Please input an integer between 1 and 18"
+    }
 })
+
+resetButtonOne.addEventListener('click', function() {
+    document.getElementById("input1").value = ''
+    outputOne.innerHTML = '';
+});
