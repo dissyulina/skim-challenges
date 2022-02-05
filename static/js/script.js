99420257f2ed055789f72fa05de61239a067b57e
Dissy Ulina <dissyulina@gmail.com>
2022-02-05 16:03:38 +0000

    Add basic functionality for challenge two

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,11 @@ let outputOne = document.getElementById("output1");
 let calculateButtonOne = document.getElementById("calculate-button1");
 let resetButtonOne = document.getElementById("reset-button1");
 
+// Declare variables from Challenge #2
+let inputTwo = document.getElementById("input2");
+let outputTwo = document.getElementById("output2");
+let calculateButtonTwo = document.getElementById("calculate-button2");
+
 // Challenge #1 Functionality
 function multiplyByDecrement(input) {
     let result = 1
@@ -26,4 +31,25 @@ calculateButtonOne.addEventListener('click', function() {
 resetButtonOne.addEventListener('click', function() {
     document.getElementById("input1").value = ''
     outputOne.innerHTML = '';
+});
+
+// Challenge #2 Functionality
+function skimLoop(input) {
+    let output = [];
+    for (let i=1 ; i<=input; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            output.push("SKIM is a great place to work!!") 
+        } else if (i % 3 == 0) {
+            output.push("SKIM")
+        } else if (i % 5 == 0) {
+            output.push("is a great place to work!!")
+        } else {
+            output.push(i)
+        }
+    }
+    return output.join(', ');
+}
+
+calculateButtonTwo.addEventListener('click', function() {
+    outputTwo.innerHTML = skimLoop(inputTwo.value);
 });
