1d0c863b29b52e375d9f5250a70a9d8088e17a8f
Dissy Ulina <dissyulina@gmail.com>
2022-02-06 11:06:53 +0000

    Run code through validator, tidy up codes

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,33 @@
 // Declare variables for Challenge #1
-let inputOne = document.getElementById("input1");
-let outputOne = document.getElementById("output1");
-let calculateButtonOne = document.getElementById("calculate-button1");
-let inputValidationOne = document.getElementById("input-validation1");
+const inputOne = document.getElementById("input1");
+const outputOne = document.getElementById("output1");
+const calculateButtonOne = document.getElementById("calculate-button1");
+const inputValidationOne = document.getElementById("input-validation1");
 
 // Declare variables for Challenge #2
-let inputTwo = document.getElementById("input2");
-let outputTwo = document.getElementById("output2");
-let calculateButtonTwo = document.getElementById("calculate-button2");
-let inputValidationTwo = document.getElementById("input-validation2");
+const inputTwo = document.getElementById("input2");
+const outputTwo = document.getElementById("output2");
+const calculateButtonTwo = document.getElementById("calculate-button2");
+const inputValidationTwo = document.getElementById("input-validation2");
 
-let resetButton = document.getElementsByClassName("reset-button");
+const resetButton = document.getElementsByClassName("reset-button");
 
 // Challenge #1 Functionality
 function multiplyByDecrement(input) {
-    let result = 1
+    let result = 1;
     for (let i=input ; i>0; i--) {
-        result *= i
+        result *= i;
     }
-    return result
+    return result;
 }
 
 calculateButtonOne.addEventListener('click', function() {
-    let inputValue = Number(inputOne.value);
+    const inputValue = Number(inputOne.value);
     // Validate input, an integer between 1-18
     if ((inputValue >= 1) && (inputValue <= 18) && (Number.isInteger(inputValue))) {
         outputOne.innerHTML = multiplyByDecrement(inputValue);
     } else {
-        inputValidationOne.innerHTML = "Please input an integer between 1 and 18"
+        inputValidationOne.innerHTML = "Please input an integer between 1 and 18";
     }
 });
 
@@ -36,13 +36,13 @@ function skimLoop(input) {
     let output = [];
     for (let i=1 ; i<=input; i++) {
         if ((i % 3 == 0) && (i % 5 == 0)) {
-            output.push("SKIM is a great place to work!!") 
+            output.push("SKIM is a great place to work!!");
         } else if (i % 3 == 0) {
-            output.push("SKIM")
+            output.push("SKIM");
         } else if (i % 5 == 0) {
-            output.push("is a great place to work!!")
+            output.push("is a great place to work!!");
         } else {
-            output.push(i)
+            output.push(i);
         }
     }
     return output.join(', ');
@@ -52,14 +52,14 @@ calculateButtonTwo.addEventListener('click', function() {
     let inputValueTwo = Number(inputTwo.value);
     // Validate input, has to be an integer
     if (Number.isInteger(inputValueTwo)) {
-        outputTwo.innerHTML = skimLoop(inputTwo.value)
+        outputTwo.innerHTML = skimLoop(inputTwo.value);
     } else {
-        inputValidationTwo.innerHTML = "Please input an integer"
+        inputValidationTwo.innerHTML = "Please input an integer";
     }
 });
 
 // Reset button for both challenges
-for(var i = 0; i < resetButton.length; i++) {
+for(let i = 0; i < resetButton.length; i++) {
     resetButton[i].addEventListener('click', function() {
         this.parentNode.parentNode.children[2].value = '';
         this.parentNode.parentNode.children[3].innerHTML = '';
